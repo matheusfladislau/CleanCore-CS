8147e1e6df8ba671829225618789849c59a08dee
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-03-26 13:28:39 -0300

    feat(Repositores): added entities' repositories

---
+++ CleanCore.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using CleanCore.Domain.Entities;
+using CleanCore.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+namespace CleanCore.Infra.Data.Repositories;
+public class ProductRepository : IProductRepository {
+    private ApplicationDbContext _context;
+    public ProductRepository(ApplicationDbContext context) {
+        this._context = context;
+    }
+
+    public async Task<Product> CreateAsync(Product product) {
+        _context.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<IEnumerable<Product>> GetByCategoryAsync(int? categoryId) {
+        return await _context.Products
+            .Where(x => x.CategoryId == categoryId)
+            .ToListAsync();
+    }
+
+    public async Task<Product> GetByIdAsync(int? id) {
+        return await _context.Products.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Product>> GetBySupplierAsync(int? supplierId) {
+        return await _context.Products
+            .Where(x => x.SupplierId == supplierId)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsAsync() {
+        return await _context.Products
+            .ToListAsync();
+    }
+
+    public async Task<Product> GetProductCategoryAsync(int? id) {
+        return await _context.Products.Include(x => x.Category)
+            .SingleOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Product> RemoveAsync(Product product) {
+        _context.Remove(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> UpdateAsync(Product product) {
+        _context.Update(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+}
---
+++ CleanCore.Infra.Data/Repositories/SupplierRepository.cs
@@ -0,0 +1,37 @@
+﻿using CleanCore.Domain.Entities;
+using CleanCore.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanCore.Infra.Data.Repositories; 
+public class SupplierRepository : ISupplierRepository {
+    private ApplicationDbContext _context;
+
+    public SupplierRepository(ApplicationDbContext context) {
+        this._context = context;
+    }
+    public async Task<Supplier> CreateAsync(Supplier supplier) {
+        _context.Add(supplier);
+        await _context.SaveChangesAsync();
+        return supplier;
+    }
+
+    public async Task<Supplier> GetByIdAsync(int? id) {
+        return await _context.Suppliers.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Supplier>> GetSuppliersAsync() {
+        return await _context.Suppliers.ToListAsync();
+    }
+
+    public async Task<Supplier> RemoveAsync(Supplier supplier) {
+        _context.Remove(supplier);
+        await _context.SaveChangesAsync();
+        return supplier;
+    }
+
+    public async Task<Supplier> UpdateAsync(Supplier supplier) {
+        _context.Update(supplier);
+        await _context.SaveChangesAsync();
+        return supplier;
+    }
+}
