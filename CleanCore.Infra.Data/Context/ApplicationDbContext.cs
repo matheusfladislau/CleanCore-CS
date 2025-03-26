26b8c015180001b4481397e33d611a7cdd755d6d
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-03-26 12:25:39 -0300

    build: add ApplicationDbContext

---
+++ CleanCore.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CleanCore.Domain.Entities;
+
+namespace CleanCore.Infra.Data.Context;
+
+public class ApplicationDbContext : DbContext {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
