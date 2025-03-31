60a536a7904b1e20cd561a17df265d92c2b41e54
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-03-31 12:40:19 -0300

    feat(Application): added AutoMapper profiles

---
+++ CleanCore.Application/Mappings/ProductProfile.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+using CleanCore.Application.DTOs;
+using CleanCore.Domain.Entities;
+
+namespace CleanCore.Application.Mappings; 
+public class ProductProfile : Profile {
+    public ProductProfile() {
+        CreateMap<Product, ProductDTO>().ReverseMap();
+    }
+}
---
+++ CleanCore.Application/Mappings/SupplierProfile.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+using CleanCore.Application.DTOs;
+using CleanCore.Domain.Entities;
+
+namespace CleanCore.Application.Mappings; 
+public class SupplierProfile : Profile {
+    public SupplierProfile() {
+        CreateMap<Supplier, SupplierDTO>().ReverseMap();
+    }
+}
