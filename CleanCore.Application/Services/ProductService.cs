7d2e979bc0f61f9a53a0203b289a5e5d0e5cb7cb
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-03-31 14:37:39 -0300

    feat(Application): Services' implementations

---
+++ CleanCore.Application/Services/ProductService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CleanCore.Application.DTOs;
+using CleanCore.Application.Interfaces;
+using CleanCore.Domain.Entities;
+
+namespace CleanCore.Application.Services;
+public class ProductService : IProductService {
+    private readonly IProductRepository _repository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository repository,
+        IMapper mapper) {
+        this._repository = repository;
+        this._mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ProductDTO>> GetProducts() {
+        var productsEntities = await _repository.GetProductsAsync();
+        return _mapper.Map<IEnumerable<ProductDTO>>(productsEntities);
+    }
+
+    public async Task<ProductDTO> GetById(int? id) {
+        var productEntity = await _repository.GetByIdAsync(id);
+        return _mapper.Map<ProductDTO>(productEntity);
+    }
+    
+    public async Task CreateAsync(ProductDTO product) {
+        var productEntity = _mapper.Map<Product>(product);
+        await _repository.CreateAsync(productEntity);
+    }
+
+    public async Task RemoveAsync(ProductDTO product) {
+        var productEntity = _mapper.Map<Product>(product);
+        await _repository.RemoveAsync(productEntity);
+    }
+
+    public async Task UpdateAsync(ProductDTO product) {
+        var productEntity = _mapper.Map<Product>(product);
+        await _repository.UpdateAsync(productEntity);
+    }
+
+    public async Task<IEnumerable<ProductDTO>> GetBySupplier(int? supplierId) {
+        var productsEntities = await _repository.GetBySupplierAsync(supplierId);
+        return _mapper.Map<IEnumerable<ProductDTO>>(productsEntities);
+    }
+
+    public async Task<IEnumerable<ProductDTO>> GetByCategory(int? categoryId) {
+        var productsEntities = await _repository.GetByCategoryAsync(categoryId);
+        return _mapper.Map<IEnumerable<ProductDTO>>(productsEntities);
+    }
+
+    public async Task<ProductDTO> GetProductCategory(int? id) {
+        var productEntity = await _repository.GetProductCategoryAsync(id);
+        return _mapper.Map<ProductDTO>(productEntity);
+    }
+}
---
+++ CleanCore.Application/Services/SupplierService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using CleanCore.Application.DTOs;
+using CleanCore.Application.Interfaces;
+using CleanCore.Domain.Entities;
+
+namespace CleanCore.Application.Services; 
+public class SupplierService : ISupplierService {
+    private readonly ISupplierRepository _repository;
+    private readonly IMapper _mapper;
+
+    public SupplierService(ISupplierRepository repository,
+        IMapper mapper) {
+        this._repository = repository;
+        this._mapper = mapper;
+    }
+
+    public async Task<IEnumerable<SupplierDTO>> GetSuppliers() {
+        var suppliersEntities = await _repository.GetSuppliersAsync();
+        return _mapper.Map<IEnumerable<SupplierDTO>>(suppliersEntities);
+    }
+
+    public async Task<SupplierDTO> GetById(int? id) {
+        var supplierEntity = await _repository.GetByIdAsync(id);
+        return _mapper.Map<SupplierDTO>(supplierEntity);
+    }
+
+    public async Task Add(SupplierDTO supplier) {
+        var supplierEntity = _mapper.Map<Supplier>(supplier);
+        await _repository.CreateAsync(supplierEntity);
+    }
+
+    public async Task Remove(SupplierDTO supplier) {
+        var supplierEntity = _mapper.Map<Supplier>(supplier);
+        await _repository.RemoveAsync(supplierEntity);
+    }
+
+    public async Task Update(SupplierDTO supplier) {
+        var supplierEntity = _mapper.Map<Supplier>(supplier);
+        await _repository.UpdateAsync(supplierEntity);
+    }
+}
