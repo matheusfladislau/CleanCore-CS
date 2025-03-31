f0e5398db228a126f4f50be9e852bfb87e2d17e1
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-03-31 14:37:18 -0300

    feat(Application): Services' interfaces

---
+++ CleanCore.Application/Interfaces/ISupplierService.cs
@@ -0,0 +1,11 @@
+﻿using CleanCore.Application.DTOs;
+
+namespace CleanCore.Application.Interfaces;
+public interface ISupplierService {
+    Task<IEnumerable<SupplierDTO>> GetSuppliers();
+    Task<SupplierDTO> GetById(int? id);
+
+    Task Add(SupplierDTO supplier);
+    Task Remove(SupplierDTO supplier);
+    Task Update(SupplierDTO supplier);
+}
