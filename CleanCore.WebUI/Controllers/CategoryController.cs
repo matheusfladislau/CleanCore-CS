f0daa22de806df539c280af386b21b7ebd6354bb
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-04-04 14:21:05 -0300

    feat(WebUI): added controllers for testing

---
+++ CleanCore.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using CleanCore.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanCore.WebUI.Controllers; 
+public class CategoryController : Controller {
+    private readonly ICategoryService _service;
+
+    public CategoryController(ICategoryService service) {
+        this._service = service; 
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Index() {
+        var result = await _service.GetCategories();
+        return View(result);
+    }
+}
---
+++ CleanCore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using CleanCore.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanCore.WebUI.Controllers; 
+public class ProductController : Controller {
+    private readonly IProductService _service;
+
+    public ProductController(IProductService service) {
+        this._service = service;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Index() {
+        var result = await _service.GetProducts();
+        return View(result);
+    }
+}
